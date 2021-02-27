6bbec8d5c96ff5102afb88311595b99389eb3c17
Luc Perkins <luc@timber.io>
2021-02-26 18:17:33 -0800

    Add some basic stats

--- src/bench.rs
+++ src/bench.rs
@@ -1,5 +1,7 @@
+use average::{define_histogram, Histogram10, Max, Mean, Min, Quantile, Variance};
 use chrono::{DateTime, Utc};
 use serde_json::Value;
+use std::collections::HashMap;
 
 #[derive(sqlx::FromRow)]
 pub struct Bench {
@@ -19,5 +21,33 @@ impl Bench {
             metadata,
             results,
         }
+    }
+
+    fn min(&self) -> f64 {
+        let m: Min = self.results.iter().collect();
+        m.min()
+    }
+
+    fn max(&self) -> f64 {
+        let m: Max = self.results.iter().collect();
+        m.max()
+    }
+
+    fn mean(&self) -> f64 {
+        let m: Mean = self.results.iter().collect();
+        m.mean()
+    }
+
+    fn variance(&self) -> f64 {
+        let v: Variance = self.results.iter().collect();
+        v.sample_variance()
+    }
+
+    fn histogram(&self) -> Histogram10 {
+        let mut hist = Histogram10::with_const_width(self.min(), self.max());
+        self.results.iter().for_each(|i| {
+            let _ = hist.add(*i);
+        });
+        hist
     }
 }
